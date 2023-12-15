9630118e7e14f30355deef7aed442bf56a78bbbe
Matthias Richter <matthias.richter@slub-dresden.de>
2023-12-15 17:45:14 +0100

    Fix extension structure

---
+++ ext_localconf.php
@@ -0,0 +1,7 @@
+<?php
+defined('TYPO3') or die('Access denied.');
+
+/***************
+ * PageTS
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:mpdb_web/Configuration/TsConfig/page.tsconfig">');
