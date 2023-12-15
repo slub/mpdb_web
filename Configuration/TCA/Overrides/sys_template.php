179106b6d3f4158c11509cab74560d3818d30cec
Matthias Richter <matthias.richter@slub-dresden.de>
2023-12-15 17:44:25 +0100

    Fix extension name

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -3,17 +3,14 @@ defined('TYPO3_MODE') || die();
 
 call_user_func(function()
 {
-    /**
-     * Temporary variables
-     */
     $extensionKey = 'mpdb_web';
 
     /**
-     * Default TypoScript for GuvSitepackage
+     * Default TypoScript
      */
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
         $extensionKey,
         'Configuration/TypoScript',
-        'mpdb_web'
+        'Music Publisher Database Template'
     );
 });
